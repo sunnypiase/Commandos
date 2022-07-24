8152b174494bc77c7d2d1ab703c3da62ff8e41ae
SunnyPiase <petya5671@gmail.com>
2022-07-24 23:27:41 +0300

    Storage remake to typle

--- Commandos/Commandos/Storage/ProductStorage.cs
+++ Commandos/Commandos/Storage/ProductStorage.cs
@@ -4,12 +4,18 @@ using System.Collections;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
+using Commandos.Models.Products.General;
+using Commandos.TxtSerialize;
+using System.Collections;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace Commandos.Storage
 {
     [Serializable]
-    public class ProductStorage<T> : IList<T>, IXmlSerializable
+    public class ProductStorage<T> : IList<(T Product, int Amount)>, IXmlSerializable
         where T : class, IProduct
     {
 
@@ -17,7 +23,7 @@ namespace Commandos.Storage
         private static ProductStorage<T> _instance;
         [XmlIgnore]
         public static ProductStorage<T> Instance => _instance is null ? _instance = new() : _instance;
-        private List<T> _products;
+        private List<(T Product, int Amount)> _products;
         /// <summary>
         /// перевіряє чи підпадає під задані умови продукт перед його додаванням
         /// </summary>
@@ -26,9 +32,10 @@ namespace Commandos.Storage
         /// у випадку, коли продукт не підпадає під умови додавання викликається ця подія, повинна записувати лог у файл
         /// </summary>
         public event Action<string>? OnBadProductLogger;
-        public double Pirice => _products.Select(product => product.Price).Sum();
-        public double MaxPrice => _products.Select(product => product.Price).Max();
+        public double Pirice => _products.Select(product => product.Product.Price).Sum();
+        public double MaxPrice => _products.Select(product => product.Product.Price).Max();
         #endregion
+
         #region Ctors
         private ProductStorage()
         {
@@ -36,22 +43,36 @@ namespace Commandos.Storage
         }
 
         #endregion
+
         #region IList
-        public T this[int index]
+
+        public (T Product, int Amount) this[int index]
         {
             get => _products[index];
-            set => _products[index] = (T)value.Clone();
+            set => _products[index] = value;
         }
 
         public int Count => _products.Count;
 
         public bool IsReadOnly => false;
 
-        public void Add(T item)
+        public void Add((T Product, int Amount) item)
+        {
+            if (OnProductPreAddFaceControl?.Invoke(item.Product) ?? true)
+            {
+                _products.Add(item);
+            }
+            else
+            {
+                OnBadProductLogger?.Invoke(new TxtSerializer().Serialize(item.Product) + "<Describe : Продукт не підпадає під умови додавання>;");
+            }
+        }
+
+        public void AddOneProduct(T item, int count = 1)
         {
             if (OnProductPreAddFaceControl?.Invoke(item) ?? true)
             {
-                _products.Add((T)item.Clone());
+                _products.Add((item, count));
             }
             else
             {
@@ -64,39 +85,43 @@ namespace Commandos.Storage
             _products.Clear();
         }
 
-        public bool Contains(T item)
+        public bool Contains((T Product, int Amount) item)
         {
             return _products.Contains(item);
         }
+        public bool Contains(T Product)
+        {
+            return _products.Select(x => x.Product).Contains(Product);
+        }
 
-        public void CopyTo(T[] array, int arrayIndex)
+        public void CopyTo((T Product, int Amount)[] array, int arrayIndex)
         {
             _products.CopyTo(array, arrayIndex);
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public IEnumerator<(T Product, int Amount)> GetEnumerator()
         {
             return _products.GetEnumerator();
         }
 
-        public int IndexOf(T item)
+        public int IndexOf((T Product, int Amount) item)
         {
             return _products.IndexOf(item);
         }
 
-        public void Insert(int index, T item)
+        public void Insert(int index, (T Product, int Amount) item)
         {
-            if (OnProductPreAddFaceControl?.Invoke(item) ?? true)
+            if (OnProductPreAddFaceControl?.Invoke(item.Product) ?? true)
             {
-                _products.Insert(index, (T)item.Clone());
+                _products.Insert(index, item);
             }
             else
             {
-                OnBadProductLogger?.Invoke(new TxtSerializer().Serialize(item) + "<Describe : Продукт не підпадає під умови додавання>;");
+                OnBadProductLogger?.Invoke(new TxtSerializer().Serialize(item.Product) + "<Describe : Продукт не підпадає під умови додавання>;");
             }
         }
 
-        public bool Remove(T item)
+        public bool Remove((T Product, int Amount) item)
         {
             return (_products.Remove(item));
         }
@@ -107,25 +132,26 @@ namespace Commandos.Storage
         }
 
         #endregion
+
         #region Methods
 
         public IEnumerable<G> GetAll<G>() where G : T
         {
-            foreach (T item in _products)
+            foreach (var item in _products)
             {
-                if (item is G result)
+                if (item.Product is G result)
                 {
                     yield return result;
                 }
             }
         }
-        public IEnumerable<G> GetAll<G>(Predicate<G> predicate) where G : T
+        public IEnumerable<(G Product, int Amount)> GetAll<G>(Predicate<(T Product, int Amount)> predicate) where G : T
         {
-            foreach (T item in _products)
+            foreach (var item in _products)
             {
-                if (item is G result && predicate(result))
+                if (item.Product is G result && predicate(item) )
                 {
-                    yield return result;
+                    yield return (result, item.Amount);
                 }
             }
         }
@@ -133,19 +159,20 @@ namespace Commandos.Storage
         {
             _products.Sort();
         }
-        public void Sort(IComparer<T> comparer)
+        public void Sort(IComparer<(T, int)> comparer)
         {
             _products.Sort(comparer);
         }
 
         #endregion
+
         #region ObjectOverrides
         public override string ToString()
         {
             StringBuilder sb = new();
-            foreach (IProduct product in _products)
+            foreach (var product in _products)
             {
-                sb.AppendLine(product.ToString());
+                sb.AppendLine(product.Item1.ToString() + $"[{product.Item2} points]");
             }
             return sb.ToString();
         }
@@ -160,20 +187,20 @@ namespace Commandos.Storage
                 XmlSerializer serial = new XmlSerializer(type);
 
                 reader.ReadStartElement("IProduct");
-                this.Add((IProduct)serial.Deserialize(reader) as T);
+                this.AddOneProduct((IProduct)serial.Deserialize(reader) as T);
                 reader.ReadEndElement();
             }
             reader.ReadEndElement();
         }
         public void WriteXml(XmlWriter writer)
         {
-            foreach (IProduct product in this)
+            foreach (var product in this)
             {
                 writer.WriteStartElement("IProduct");
                 writer.WriteAttributeString
-                ("AssemblyQualifiedName", product.GetType().AssemblyQualifiedName);
-                XmlSerializer xmlSerializer = new XmlSerializer(product.GetType());
-                xmlSerializer.Serialize(writer, product);
+                ("AssemblyQualifiedName", product.Item1.GetType().AssemblyQualifiedName);
+                XmlSerializer xmlSerializer = new XmlSerializer(product.Item1.GetType());
+                xmlSerializer.Serialize(writer, product.Item1);
                 writer.WriteEndElement();
             }
         }
