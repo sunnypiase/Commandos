9dec78848bbfa4126523f49d72ad6daec8d6ecec
Олег Мельников <olegm72@gmail.com>
2022-07-24 17:09:54 +0200

    AuthorizationService added

---
+++ Commandos/CommandosLogic/Models/Users/UsersRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Commandos.Models.Users
+{
+    [DataContract]
+    internal class UsersRepository
+    {
+        [DataMember]
+        private List<IUser> users;
+
+        [XmlIgnore]
+        private static UsersRepository instance;
+
+        public static UsersRepository GetInstance() => instance is null ? instance = new UsersRepository() : instance;
+
+        public List<IUser> AllUsers() => users;
+
+        private UsersRepository()
+        {
+            ReadUsersFromFile();
+            if (users is null) // this is a strange error but to avoid such situation we create new list
+                users = new List<IUser>();
+        }
+
+        public void AddUser(user: IUser)
+        {
+            users.Add(user);
+        }
+
+        public void RemoveUser(user: IUser)
+        {
+            users.Remove(user);
+        }
+
+        public void ReadUsersFromFile()
+        {
+            DownloaderProcessor.GetUserDataSerializer(new XmlSerialization<UsersRepository>).Load(instance);
+        }
+
+        public void SaveUsersToFile()
+        {
+            DownloaderProcessor.GetUserDataSerializer(new XmlSerialization<UsersRepository>).Save(instance);
+        }
+    }
+}
---
+++ Commandos/CommandosLogic/Services/AuthorizationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Commandos.Models.Users;
+
+namespace Commandos.Services
+{
+    internal class AuthorizationService
+    {
+        private UsersRepository usersData;
+
+        public AuthorizationService()
+        {
+            usersData = UsersRepository.GetInstance();
+        }
+
+        public IUser LoginRoutine()
+        {
+            return null;
+        }
+
+
+    }
+}
