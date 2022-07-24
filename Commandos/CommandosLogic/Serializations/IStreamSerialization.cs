cff136bd955627b6a5ac5818ea068adadec2475a
Misha <misha.pavlenko.1111@gmail.com>
2022-07-24 18:49:33 +0300

    Serialization added

---
+++ Commandos/CommandosLogic/Serializations/IStreamSerialization.cs
@@ -0,0 +1,8 @@
+﻿namespace Commandos.Serialize
+{
+    public interface IStreamSerialization<T> where T : class
+    {
+        public void Serialize(T obj, Stream stream);
+        public T Deserialize(Stream stream);
+    }
+}
