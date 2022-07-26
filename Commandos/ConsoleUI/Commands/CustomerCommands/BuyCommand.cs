0c19807f9c0127579a53374153e43b345cce8e82
KataytsevYuriy <yuriy.kataytsev@gmail.com>
2022-07-26 17:55:24 +0300

    Add Buy command

---
+++ Commandos/ConsoleUI/Commands/CustomerCommands/BuyCommand.cs
@@ -0,0 +1,34 @@
+﻿using Commandos.Models.Carts;
+using ConsoleUI.Menu.MenuTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Commands.CustomerCommands
+{
+    internal class BuyCommand : CommandOn<Cart>
+    {
+        private string title;
+        public BuyCommand(string title)
+        {
+            this.title = title;
+        }
+        public override object Clone()
+        {
+            return new BuyCommand(title);
+        }
+        public override ICollection<IMenuElement>? Execute()
+        {
+            List<IMenuElement> elements = new();
+            Buy buy = new Buy();
+            Check check = buy.BuyCart(CartsRepository.GetInstance().GetCart(commandTarget.Id));
+            elements.Add(new InfoElement("Successfullly buyed!"));
+            elements.Add(new InfoElement("Your check:"));
+            elements.Add(new InfoElement(check.ToString()));
+            elements.Add(new SelectableElement("back to home", "0", new BackToHome()));
+            return elements;
+        }
+    }
+}
